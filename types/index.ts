58245c30bc8e6ae1c5b83e27fab269ae7fb93021
HHao <work@hhao.wang>
2024-05-07 17:12:15 +0800

    导出ValidationError

--- types/index.ts
+++ types/index.ts
@@ -2,5 +2,4 @@ export { AbstractQuestionGroup } from "./AbstractQuestionGroup";
 export { AbstractSurvey } from "./AbstractSurvey";
 export { AbstractQuestion, QuestionType, QuestionInfo } from "./AbstractQuestion";
 export { AbstractPage } from "./AbstractPage";
-export { KeyValuePair } from "./Common";
-
+export { KeyValuePair, ValidationError } from "./Common";
